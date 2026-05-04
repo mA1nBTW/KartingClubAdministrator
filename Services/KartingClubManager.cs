053808d4e54308ab84da8e30bef1e7efcac2529c
Павленко Михайло <mykhailo.pavlenko@nure.ua>
2026-05-04 21:50:25 +0300

    Service KartingClubManager added

---
+++ Services/KartingClubManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KartingClubApp.Services
+{
+    /// <summary>
+    /// Головний керуючий клас системи, реалізований за патерном "Фасад".
+    /// Координує роботу з колекціями гонщиків та заїздів, виконує валідацію,
+    /// розрахунок статистики та збереження даних у форматі JSON.
+    /// </summary>
+    public class KartingClubManager
+    {
+    }
+}
